469b28f21c772a3eff7c37b4d7edd31e534f507c
Yasser Shalash <yasservanshalash@gmail.com>
2023-06-22 20:13:45 +0200

    lets go

--- src/app/souvenirs/page.tsx
+++ src/app/souvenirs/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <header>
+        <h1 className='text-white uppercase text-center font-bold'>Souvenirs</h1>
+        <p className='text-[#999] text-center pt-[200px]'>be patient, they&apos;re coming...</p>
+    </header>
   )
 }
 
